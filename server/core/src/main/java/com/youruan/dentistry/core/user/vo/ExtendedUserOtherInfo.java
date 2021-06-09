9561ef717f300f1bf4aed72fe2656519066c8393
Edenkhan <643868796@qq.com>
2021-06-09 18:23:50 +0800

    houpo

---
+++ server/core/src/main/java/com/youruan/dentistry/core/user/vo/ExtendedUserOtherInfo.java
@@ -0,0 +1,9 @@
+package com.youruan.dentistry.core.user.vo;
+
+import com.youruan.dentistry.core.user.domain.UserOtherInfo;
+
+public class ExtendedUserOtherInfo
+        extends UserOtherInfo
+{
+    private final static long serialVersionUID = 1L;
+}
--- server/portal/src/main/java/com/youruan/dentistry/portal/wx/user/UserController.java
+++ server/portal/src/main/java/com/youruan/dentistry/portal/wx/user/UserController.java
@@ -3,9 +3,13 @@ package com.youruan.dentistry.portal.wx.user;
 import com.google.common.collect.ImmutableMap;
 import com.youruan.dentistry.core.base.utils.BeanMapUtils;
 import com.youruan.dentistry.core.user.domain.RegisteredUser;
+import com.youruan.dentistry.core.user.domain.UserOtherInfo;
 import com.youruan.dentistry.core.user.service.RegisteredUserService;
+import com.youruan.dentistry.core.user.service.UserOtherInfoService;
+import com.youruan.dentistry.core.user.vo.UserAllInfoVo;
 import com.youruan.dentistry.portal.base.interceptor.RequiresAuthentication;
 import com.youruan.dentistry.portal.base.utils.SessionUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,21 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final RegisteredUserService registeredUserService;
+    private final UserOtherInfoService userOtherInfoService;
 
-    public UserController(RegisteredUserService registeredUserService) {
+    public UserController(RegisteredUserService registeredUserService, UserOtherInfoService userOtherInfoService) {
         this.registeredUserService = registeredUserService;
+        this.userOtherInfoService = userOtherInfoService;
     }
-
 
     @GetMapping("/get")
     @RequiresAuthentication
     public ResponseEntity<?> get() {
         Long userId = SessionUtils.getAuthenticated();
-        RegisteredUser user = registeredUserService.get(userId);
-        return ResponseEntity.ok(ImmutableMap.builder()
-                .put("user", BeanMapUtils.pick(user,
-                        "id","avatar","nickname","phoneNumber"))
-                .build());
+        RegisteredUser userBasicInfo = registeredUserService.get(userId);
+        UserOtherInfo userOtherInfo = userOtherInfoService.get(userId);
+        UserAllInfoVo userAllInfoVo = new UserAllInfoVo();
+        BeanUtils.copyProperties(userBasicInfo,userAllInfoVo);
+        BeanUtils.copyProperties(userOtherInfo,userAllInfoVo);
+        return ResponseEntity.ok(BeanMapUtils.pick(userAllInfoVo,
+                "id","avatar","nickname","phoneNumber"));
     }
 
     /**
